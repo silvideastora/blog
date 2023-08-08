637ca849c66754d67f15d422a85ea5f55e7acbc1
Silvia rodriguez <silvideastora18@gmail.com>
2023-08-08 02:28:23 -0600

    add new project with typescript

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -24,7 +24,7 @@ function Projects() {
           </div>
           <a href="https://insta-shop.vercel.app/" className="btn btn-link">Go to InstaShop</a>
         </div>
-    </div>
+      </div>
     <div className="card w-100 mb-3 border-primary">
         <h4 className="card-header">Home Deco</h4>
         <div className="card-body">
@@ -41,7 +41,26 @@ function Projects() {
           </div>
           <a href="https://vermillion-concha-eb4fb8.netlify.app/" className="btn btn-link">Go to Home Deco</a>
         </div>
-    </div>   
+      </div>
+      <div className="card w-100 mb-3 border-primary">
+        <h4 className="card-header">To Do with TypeScript</h4>
+        <div className="card-body">
+          <h6 className="card-title">A feature-rich to-do list application built using TypeScrip</h6>
+          <p className="card-text fs-6 fw-light">
+            Task Creation: Easily add new tasks by entering a task title and optional details.
+            Marking Complete: Mark tasks as complete to visually track your progress.
+            Filters: Quickly find tasks using search and apply filters based on categories or completion status.
+            Dark Mode: Enjoy a comfortable user experience with a dark mode option.
+          </p>
+          <div className="d-flex justify-content-evenly pb-4">
+            <span className="badge text-bg-primary d-inline-flex p-3 rounded-pill fw-light">TypeScript</span>
+            <span className="badge text-bg-primary d-inline-flex p-3 rounded-pill fw-light">Vite.js</span>
+            <span className="badge text-bg-primary d-inline-flex p-3 rounded-pill fw-light">React.js</span>
+            <span className="badge text-bg-primary d-inline-flex p-3 rounded-pill fw-light">Autoanimate</span>
+          </div>
+          <a href="https://insta-shop.vercel.app/" className="btn btn-link">Go to To Do with TypeScript</a>
+        </div>
+      </div>   
     </div>
   )
 }
